92c0bce2e12b045b5a637f861f593a7004dfecb5
Benjamin Sherman <benjamin@holyarmy.org>
2023-05-18 19:58:07 -0500

    chore: add new ublue-os-akmods public key
    Since we'll cutover to new signing key on June 17, aid transition by
    encouraging installation of both new and old keys during ISO install.

--- installer/kickstart/post-install-nvidia.sh
+++ installer/kickstart/post-install-nvidia.sh
@@ -4,4 +4,5 @@ set -oue pipefail
 
 source /run/install/repo/kickstart/ublue-os-env-vars
 
+/run/install/repo/kickstart/enroll-secureboot-key.sh "${SECUREBOOT_KEY_OLD}" "${ENROLLMENT_PASSWORD}"
 /run/install/repo/kickstart/enroll-secureboot-key.sh "${SECUREBOOT_KEY}" "${ENROLLMENT_PASSWORD}"
